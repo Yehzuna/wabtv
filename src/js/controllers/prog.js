3b8276506a331ae6cfd9e1ad9a9138ff5ed90d8a
Thomas BORUSZEWSKI <t.boruszewski@groupecerise.fr>
2017-01-25 13:59:38 +0100

    Update schedule json and template

--- src/js/controllers/prog.js
+++ src/js/controllers/prog.js
@@ -2,18 +2,16 @@ app.controller('progCtrl', function ($scope, $http, $location, api) {
     console.log('progCtrl');
 
     $scope.hours = [
-        "14:00 20:00",
-        "20:00 22:00",
-        "22:00 00:00",
-        "00:00 02:00"
+        "14:00 - 20:00",
+        "20:00 - 22:00",
+        "22:00 - 00:00",
+        "00:00 - 02:00"
     ];
 
-    $scope.schedule = [];
+    $scope.schedules = [];
     api.prog().then(function (response) {
-        $scope.schedule = response.data;
-        console.log($scope.schedule);
-    }).catch(function () {
-        $scope.schedule = [];
+        $scope.schedules = response.data;
+        console.log($scope.schedules);
     });
 
 });
