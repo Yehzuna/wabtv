291cd80f86fbbaa5a120ab3a81c794dacd952981
Yehzuna <yehzuna@outlook.com>
2017-01-27 00:17:08 +0100

    Responsive schedule

--- src/js/controllers/prog.js
+++ src/js/controllers/prog.js
@@ -1,13 +1,6 @@
 app.controller('progCtrl', function ($scope, $http, $location, api) {
     console.log('progCtrl');
 
-    $scope.hours = [
-        "14:00 - 20:00",
-        "20:00 - 22:00",
-        "22:00 - 00:00",
-        "00:00 - 02:00"
-    ];
-
     $scope.schedules = [];
     api.prog().then(function (response) {
         $scope.schedules = response.data;
