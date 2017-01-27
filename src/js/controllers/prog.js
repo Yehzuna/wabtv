13cd3a7f844c93d69dd292656797188eba5bd5f0
Thomas BORUSZEWSKI <t.boruszewski@groupecerise.fr>
2017-01-27 14:12:56 +0100

    Tests to export the schedule

--- src/js/controllers/prog.js
+++ src/js/controllers/prog.js
@@ -6,5 +6,20 @@ app.controller('progCtrl', function ($scope, $http, $location, api) {
         $scope.schedules = response.data;
         console.log($scope.schedules);
     });
+    
+    $scope.export = function () {
+        var div = document.getElementById('schedule').cloneNode(true);
+        div.classList.add('export');
+
+        document.body.appendChild(div);
+
+        domtoimage.toJpeg(div, { quality: 0.95 })
+            .then(function (dataUrl) {
+                console.log('test');
+                var img = new Image();
+                img.src = dataUrl;
+                document.body.appendChild(img);
+            });
+    }
 
 });
