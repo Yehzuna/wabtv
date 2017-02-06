06e693051783c287d82aa68c5d747ab244a3abf5
Thomas BORUSZEWSKI <t.boruszewski@groupecerise.fr>
2017-02-06 14:09:52 +0100

    Twitch player tests

--- src/js/controllers/home.js
+++ src/js/controllers/home.js
@@ -1,3 +1,24 @@
 app.controller('homeCtrl', function ($scope, $http, $location, api) {
     console.log('homeCtrl');
+
+    var options = {
+        channel: "jvtv"
+        //video: "{VIDEO_ID}"
+    };
+    var player = new Twitch.Player("player", options);
+    player.setVolume(0.5);
+
+    $scope.$watch(function() {
+        return angular.element('player').is(':visible')
+    }, function(status) {
+        console.debug(status);
+        console.debug("player");
+    });
+
+    $scope.$watch(function() {
+        return angular.element('#chat').is(':visible')
+    }, function(status) {
+        console.debug(status);
+        console.debug("chat");
+    });
 });
--- src/js/directives/header.js
+++ src/js/directives/header.js
@@ -4,15 +4,10 @@ app.directive('header', function () {
         replace: true,
         templateUrl: 'tpl/header.html',
         controller: function ($scope) {
-            // Your behaviour goes here :)
-            console.log("test");
             $scope.menu = false;
-
             $scope.open = function (status) {
-                console.log(status);
                 $scope.menu = status;
             }
         }
     }
 });
-
