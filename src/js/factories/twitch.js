d79cff41f517785526ef398da467190489bbcd11
Yehzuna <yehzuna@outlook.com>
2017-02-15 22:19:19 +0100

    Add correct twitch id

--- src/js/factories/twitch.js
+++ src/js/factories/twitch.js
@@ -4,7 +4,8 @@ app.factory('twitch', function ($http) {
     data.online = function () {
         return $http({
             method: "GET",
-            url: "https://api.twitch.tv/kraken/streams/84934385",
+            //url: "https://api.twitch.tv/kraken/streams/84934385", jvtv
+            url: "https://api.twitch.tv/kraken/streams/89890241",
             headers: {
                 'Accept': 'application/vnd.twitchtv.v5+json',
                 'Client-ID': 'u2t1qbp3fv8ipbrs7nrq050bwhyc0u'
