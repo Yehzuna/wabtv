1bbda22979af984929544fde932e5c7ba40d8546
Thomas Boruszewski <t.boruszewski@groupecerise.fr>
2017-03-24 18:14:33 +0100

    Add schedule admin & login

---
+++ public/api.php
@@ -0,0 +1,34 @@
+<?php
+
+error_reporting(E_ALL);
+ini_set('display_errors', 1);
+
+$hash = md5(md5("test") . ':WabTvHash:' . md5("test"));
+var_dump($hash);
+
+$hashs = [
+    "f08b3edec66d1683968b17e74f6c3224"
+];
+
+if ($_SERVER['REQUEST_METHOD'] != 'POST') {
+    header('HTTP/1.0 401 Unauthorized');
+    exit;
+}
+
+$request = file_get_contents('php://input');
+if($data = json_decode($request, true)) {
+
+    if(!isset($data['hash']) || !in_array($data['hash'], $hashs)) {
+        header('HTTP/1.0 401 Unauthorized');
+        exit;
+    }
+
+    if(isset($data['schedule'])) {
+        file_put_contents('data/schedule.json', json_encode($data['schedule']));
+    }
+
+    exit;
+}
+
+header('HTTP/1.0 401 Unauthorized');
+exit;
--- src/js/controllers/admin.js
+++ src/js/controllers/admin.js
@@ -1,8 +1,23 @@
-app.controller('adminCtrl', function ($rootScope, $scope, api) {
+app.controller('loginCtrl', function ($rootScope, $scope, $location, api) {
     $rootScope.night = false;
 
-    var dt = new Date();
-    $scope.currentDay = dt.getDate();
+    $scope.submit = function () {
+        var hash = CryptoJS.MD5(CryptoJS.MD5($scope.login) + ':WabTvHash:' + CryptoJS.MD5($scope.password));
+        console.log(hash.toString());
+
+        api.admin({
+            hash: hash.toString()
+        }).then(function () {
+            sessionStorage.setItem('WabTvHash', hash);
+            $location.path('admin');
+        }).catch(function() {
+
+        });
+    };
+});
+
+app.controller('adminCtrl', function ($rootScope, $scope, api) {
+    $rootScope.night = false;
 
     $scope.shows = {
         'default': "Autre",
@@ -18,29 +33,57 @@ app.controller('adminCtrl', function ($rootScope, $scope, api) {
         $scope.hours.push(h + ":00");
     }
 
-
     $scope.current = [];
     $scope.schedules = [];
+    $scope.confirmation = [];
+
+    window.onbeforeunload = function () {
+        console.log($scope.schedules);
+        console.log($scope.confirmation);
+        if (!angular.equals($scope.schedules, $scope.confirmation)) {
+            return true;
+        }
+    };
 
     api.schedule().then(function (response) {
         angular.forEach(response.data, function(data) {
 
-            var t = data.date.split(/[-]/);
-            var d = new Date(Date.UTC(t[0], t[1]-1, t[2]));
-            data.date = d;
-            data.currentDay = d.getDate();
+            data.date = new Date(data.date);
 
             if($scope.current.length == 0) {
                 $scope.current = data;
             }
 
             $scope.schedules.push(data);
-        })
+        });
+
+        angular.copy($scope.schedules, $scope.confirmation);
     });
 
     $scope.submit = function () {
-        console.log($scope.current);
-        console.log($scope.schedules);
-    }
+        var hash = sessionStorage.getItem('WabTvHash');
+        api.admin({
+            hash: hash,
+            schedule: $scope.schedules
+        });
+    };
+
+    $scope.slot = function () {
+        $scope.current.slots = [];
+
+        var nb = 1;
+        if(!$scope.current.allDay) {
+            nb = 4;
+        }
+
+        for (var i = 1; i <= nb; i++) {
+            $scope.current.slots.push({
+                "streamer": "",
+                "show": "default",
+                "title": "",
+                "hour": "00:00"
+            });
+        }
+    };
 
 });
